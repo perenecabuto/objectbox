75bbc4669e181c847d81f21cff7ca25467d3b9d7
Felipe Ramos <perenecabuto@gmail.com>
2010-06-20 22:34:36 -0300

    Configuração do email de cadastro

--- app/models/user_mailer.rb
+++ app/models/user_mailer.rb
@@ -2,20 +2,20 @@ class UserMailer < ActionMailer::Base
   def signup_notification(user)
     setup_email(user)
     @subject    += 'Please activate your new account'
-    @body[:url]  = "http://YOURSITE/activate/#{user.activation_code}"
+    @body[:url]  = "http://lukazupareli.net/activate/#{user.activation_code}"
   end
 
   def activation(user)
     setup_email(user)
     @subject    += 'Your account has been activated!'
-    @body[:url]  = "http://YOURSITE/"
+    @body[:url]  = "http://lukazupareli.net/"
   end
 
   protected
     def setup_email(user)
       @recipients  = "#{user.email}"
-      @from        = "ADMINEMAIL"
-      @subject     = "[YOURSITE] "
+      @from        = "no.reply@lukazupareli.net"
+      @subject     = "[Lukazupareli] "
       @sent_on     = Time.now
       @body[:user] = user
     end
